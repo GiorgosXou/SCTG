e8470a2ac7e1128764f25d7e2570a65190073fd9
‮ <gxousos@gmail.com>
2022-12-06 05:17:34 +0200

    FIX: setup.py > install_requires +++

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ if sys.version_info.major < 3:
 setup(
     name="sctg",
     version="0.1.0",
-    description="A cross-platform screenshot app with YOLO ",
+    description="A cross-platform YOLO enhanced, tagging, screenshot app that tags\\stores detected objects within image's EXIF's UserComment entry.",
     author="George Chousos",
     author_email="gxousos@gmail.com",
-    url="https://github.com/GiorgosXou/sctg",
+    url="https://github.com/GiorgosXou/SCTG",
     packages=['sctg'],
     package_data={'sctg': ['icons/*.*', 'nn_files/*.*']},
-    install_requires=['progressbar2','python-dateutil','openpyxl','matplotlib'],
+    install_requires=['opencv-python', 'numpy', 'pyautogui', 'piexif', 'pynput', 'notify-py', 'pillow'],
     entry_points={
         'console_scripts': [
             'sctg = sctg.__init__:main'
